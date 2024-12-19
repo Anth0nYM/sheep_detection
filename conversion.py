df00da621017f9b04bc3886b212d4380f6d36d51
Anth0nYM <marquesanthony62@gmail.com>
2024-12-19 12:52:32 -0300

    refactor: conversion and crop file

--- conversion.py
+++ conversion.py
@@ -1,28 +1,32 @@
 import os
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
-INPUT_DIR = 'samples/sheeps/mov'
-OUTPUT_DIR = 'samples/sheeps/mp4'
-os.makedirs(OUTPUT_DIR, exist_ok=True)
 
+class Converter:
+    def __init__(self) -> None:
+        pass
 
-def convert_to_mp4(video_path, output_path):
-    try:
-        clip = VideoFileClip(video_path)
-        clip.write_videofile(
-            output_path,
-            codec="h264_nvenc",
-            ffmpeg_params=["-preset", "fast", "-crf", "23"]
-        )
-        clip.close()
-        print("Success")
-        return output_path
-    except Exception as e:
-        print(f"There's an error converting your video: {e}")
-        return None
+    def mov_to_mp4(self, mov_path: str, mp4_path: str) -> bool:
+        try:
+            clip = VideoFileClip(mov_path)
+            clip.write_videofile(
+                mov_path,
+                codec="h264_nvenc",
+                ffmpeg_params=["-preset", "fast", "-crf", "23"]
+            )
+            clip.close()
+            print("Success")
+            return True
+        except Exception as e:
+            print(f"There's an error converting your video: {e}")
+            return False
 
 
 if __name__ == '__main__':
+    INPUT_DIR = 'samples/sheeps/mov'
+    OUTPUT_DIR = 'samples/sheeps/mp4'
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    converter = Converter()
 
     for filename in os.listdir(INPUT_DIR):
         if filename.endswith('.mov'):
@@ -31,6 +35,6 @@ if __name__ == '__main__':
             output_path = os.path.join(OUTPUT_DIR, output_filename)
 
             print(f"Converting: {input_path} -> {output_path}")
-            convert_to_mp4(input_path, output_path)
+            converter.mov_to_mp4(input_path, output_path)
 
     print("Finished!")
